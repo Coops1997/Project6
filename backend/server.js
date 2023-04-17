f9609b94fc1572983828bd7fb594fe72da2107c2
Sam Cooper <iamsamuelcooper@googlemail.com>
2023-04-17 13:13:34 +0100

    project6

---
+++ backend/server.js
@@ -0,0 +1,50 @@
+//import the Node native HTTP package//
+const http = require('http'); 
+const app = require('./app');
+
+//The  normalizePort  function returns a valid port
+const normalizePort = val => {
+const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3000'); //run on port 3000
+app.set('port', port);
+
+//The  errorHandler  function checks for various errors
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+//create a server with express that uses app & create a constant for server calls
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+server.listen(port);
