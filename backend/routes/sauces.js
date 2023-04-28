2bda9675b9538b2edf26e6d94d9dea17b788e564
Coops1997 <83956775+Coops1997@users.noreply.github.com>
2023-04-28 17:53:19 +0100

    Update sauces.js

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+
 const saucesCtrl = require('../controllers/sauces');
+
 const auth = require('../middleware/auth');
 
 router.get('/', auth, saucesCtrl.getAllSauces);
