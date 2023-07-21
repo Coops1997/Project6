fbf9c5594bd1bbf46976cdec3e391d290c61894a
Sam Cooper <iamsamuelcooper@googlemail.com>
2023-07-21 13:51:38 +0100

    update

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -13,4 +13,4 @@ router.put('/:id', auth, multer, saucesCtrl.modifySauce);
 router.delete('/:id', auth, saucesCtrl.deleteSauce);
 router.post('/:id/like', auth,saucesCtrl.likeSauce); 
 
-module.exports = router;
+module.exports = router; 
